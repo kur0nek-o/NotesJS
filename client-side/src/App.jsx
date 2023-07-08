52b5a1f331c77ac202bc065ae75ad3b91792e27b
kur0nek-o <maulana21mh@gmail.com>
2023-07-08 08:55:25 +0700

    set up `react` + `tailwind css` Hehe

---
+++ client-side/src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div className='container m-auto'>
+      <h1
+        className='
+          text-slate-600 
+          text-center 
+          text-2xl 
+          font-bold
+        '
+      >Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
